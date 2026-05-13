70632ca5bb5edc5ac7b522240dea3b76cc2b0c90
jayesh0106patil-lab <jayesh0106patil@gmail.com>
2026-05-13 21:54:12 +0530

    Add files via upload

---
+++ Array/araaySum.java
@@ -0,0 +1,13 @@
+public class araaySum{
+  public static void main (String[] argu){
+      int[]a={1,2,3,4,10,11};
+      int sum=0;
+      
+      for(int i=0; i<a.length; i++){
+          sum=sum+a[i];
+      }
+      System.out.print(sum);
+
+      
+  }
+}
